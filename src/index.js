13c0b885fb6f383e9bbcd41716e2d0dc43542b33
Dinesh Dhumak <dineshdhumak@gmail.com>
2021-08-11 15:04:53 +0530

    Add components of  linnear buffer, Form dialogs, simple transfer list,
    linnear buffer, Form dialogs, simple transfer list

--- src/index.js
+++ src/index.js
@@ -6,12 +6,18 @@ import { BrowserRouter} from 'react-router-dom';
 import Menu from './Components/Menu.js';
 import T1 from './Components/T1.js';
 import Linnearstepper from './Components/Linnearstepper.js';
+import Simtranslist from './Components/Simtranslist.js';  
+import Linnearbuffer from './Components/Linnearbuffer.js';
+import Formdialog from './Components/Formdialog.js'
 ReactDOM.render(
    <BrowserRouter>          
     <App />    
         <Menu />  
         <T1 />    
         <Linnearstepper />   
+        <Simtranslist />
+        <Linnearbuffer />
+        <Formdialog />
           </BrowserRouter>, 
   document.getElementById('root')
 );
